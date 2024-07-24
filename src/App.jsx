2315d7100df799d82290b9e9cf20618e346e2763
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2024-07-24 12:51:49 +0600

    added all files and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
+import Carousel from "./components/Carousel";
+import Category from "./components/Category";
+import Navbar from "./components/Navbar";
+import SearchHome from "./components/SearchHome";
+
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="container-xxl bg-white p-0">
+      <Navbar />
+      <Carousel />
+      <SearchHome />
+      <Category />
+    </div>
   );
 }
 
