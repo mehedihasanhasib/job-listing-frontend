602a28d2b18c8ea269ee472a0542b975bb30da37
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2024-07-25 12:10:25 +0600

    button-primary css fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,8 @@ import Carousel from "../components/Carousel";
 import SearchHome from "../components/SearchHome";
 import Category from "../components/Category";
 import JobList from "../components/JobList";
-import { useEffect } from "react";
 
 const Home = () => {
-  useEffect(()=>{
-    console.log("rendered");
-  }, [])
   return (
     <>
       <Carousel />
