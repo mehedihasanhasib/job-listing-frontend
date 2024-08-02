61406029f409e97aea4d1a7d7b5755324317d036
Mehedi Hasan Hasib <mehedi.mg50@gmail.com>
2024-08-02 22:55:03 +0600

    separated the education part in profile

---
+++ src/components/profile/EditEducation.jsx
@@ -0,0 +1,16 @@
+import { Form, Col } from "react-bootstrap";
+
+export const EditEducation = () => {
+  return (
+    <>
+      <Col md={6}>
+        <Form>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control></Form.Control>
+          </Form.Group>
+        </Form>
+      </Col>
+    </>
+  );
+};
--- src/components/profile/EducationInfo.jsx
+++ src/components/profile/EducationInfo.jsx
@@ -1,65 +1,22 @@
-import { useState } from "react";
-import { Row, Col, Card, Button } from "react-bootstrap";
-import {
-  FaUniversity,
-  FaCalendarAlt,
-  FaGraduationCap,
-  FaEdit,
-} from "react-icons/fa";
+
+import { Row, Col, Card } from "react-bootstrap";
+import { GetEducation } from "./GetEducation";
+import { EditEducation } from "./EditEducation";
 
 const EducationInfo = () => {
-  const [input, setInput] = useState(true);
-  function getEducation(degree, institute, year, cgpa) {
-    return (
-      <>
-        <Col md={12}>
-          <div className="d-flex align-items-center justify-content-between">
-            <h6 className="text-primary m-0">{degree}</h6>
-            <Button
-              variant="link"
-              size="sm"
-              className="d-flex align-items-center"
-            >
-              <FaEdit /> Edit
-            </Button>
-          </div>
-          <p>
-            <FaUniversity className="me-2" />
-            <strong>{institute}</strong>
-          </p>
-          <p>
-            <FaCalendarAlt className="me-2" />
-            <strong>Year: {year}</strong>
-          </p>
-          <p>
-            <FaGraduationCap className="me-2" /> <strong>GPA: {cgpa}</strong>
-          </p>
-          <hr />
-        </Col>
-      </>
-    );
-  }
+  const arr = [1,2,3,4]
+
   return (
     <Col md={9}>
       <Card className="shadow-sm rounded border-0">
-        <Card.Header className="bg-primary text-white border-0">
-          Education Information
-        </Card.Header>
+        <Card.Header className="bg-primary text-white border-0">Education Information</Card.Header>
         <Card.Body>
           <Row className="mb-4">
-            {getEducation("BSC", "National University", "2020", "3.84/4.00")}
+            {/* <GetEducation degree="BSC" institute="National University" year="2020" cgpa="3.84/4.00" /> */}
+           
+              {arr.map((e,i)=> (<EditEducation key={i} />))}
+            
           </Row>
-          {/* <Row>
-        <Col>
-          <h6 className="text-primary">Skills Acquired</h6>
-          <p>
-            During my education, I gained expertise in software development, 
-            data structures, algorithms, and database management. I also 
-            developed strong problem-solving and analytical skills through 
-            various projects and coursework.
-          </p>
-        </Col>
-      </Row> */}
         </Card.Body>
       </Card>
     </Col>
